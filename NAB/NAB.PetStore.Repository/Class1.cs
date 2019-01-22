5dddb94071348c4869a1fcb52370d75c128c419e
VeritasSoftware <shantanu_negi@hotmail.com>
2019-01-22 17:43:52 +1100

    Initial commit. Created solution with console app, class library, test projects.

---
+++ NAB/NAB.PetStore.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NAB.PetStore.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ NAB/NAB.PetStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NAB.PetStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
